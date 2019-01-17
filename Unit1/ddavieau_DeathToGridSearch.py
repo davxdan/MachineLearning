9903685d810d6316d5cd9f11823461fd0ad8499c
Daniel Davieau <danieldavieau@hotmail.com>
2019-01-16 19:05:27 -0500

    sdfgfdsg
    frawafeafrgdfsdg

--- Unit1/ddavieau_DeathToGridSearch.py
+++ Unit1/ddavieau_DeathToGridSearch.py
@@ -17,28 +17,30 @@ from sklearn.model_selection import KFold
 # 5. Please set up your code to be run and save the results to the directory that its executed from
 # 6. Investigate grid search function
 
-M = np.array([[1,2],[3,4],[4,5],[4,5],[4,5],[4,5],[4,5],[4,5]])
-L = np.ones(M.shape[0])
+M = numpy.array([[1,2],[3,4],[4,5],[4,5],[4,5],[4,5],[4,5],[4,5]])
+L = numpy.random.choice([0,1],size=(M.shape[0])) #creates an array of 1's in the same as rows in M
 n_folds = 5
+data = (M, L, n_folds) #creates a tuple; a tuple is same as list but immutable (Cant be changed)
 
-data = (M, L, n_folds)
-
-def run(a_clf, data, clf_hyper={}):
+def run(a_Classifier, data, Classifier_hyper={}): # {} are dictionary and [] are for list
   M, L, n_folds = data # unpack data containter
   kf = KFold(n_splits=n_folds) # Establish the cross validation
-  ret = {} # classic explicaiton of results
-
+  Classifier_hyper= {'n_jobs': 2}
+  ret = {} # classic explicaiton of results as dictionary
   for ids, (train_index, test_index) in enumerate(kf.split(M, L)):
-    clf = a_clf(**clf_hyper) # unpack paramters into clf is they exist
-
-    clf.fit(M[train_index], L[train_index])
-
-    pred = clf.predict(M[test_index])
-
-    ret[ids]= {'clf': clf,
+        print("k fold = ", ids)
+        print("            train indexes = ", train_index)
+        print("            test indexes = ", test_index)
+        Classifier = a_Classifier(**Classifier_hyper) # unpack paramters into clf is they exist
+        Classifier.fit(M[train_index], L[train_index])
+        pred = Classifier.predict(M[test_index])
+        ret[ids]= {'Classifier': Classifier,
                'train_index': train_index,
                'test_index': test_index,
-               'accuracy': accuracy_score(L[test_index], pred)}
+               'accuracy': accuracy_score(L[test_index], pred)},
   return ret
 
-results = run(RandomForestClassifier, data, clf_hyper={})
+algorithmlist = [RandomForestClassifier,LogisticRegression]
+for algorithms in algorithmlist:
+    results = run(algorithms, data, Classifier_hyper={})
+    print(results)
