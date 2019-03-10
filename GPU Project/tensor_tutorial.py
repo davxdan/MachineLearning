4c7d2eb9ffc111b8a4d39f4e77e93c7ce5e1334e
Daniel Davieau <danieldavieau@hotmail.com>
2019-03-09 22:16:22 -0500

    Update tensor_tutorial.py

--- GPU Project/tensor_tutorial.py
+++ GPU Project/tensor_tutorial.py
@@ -22,31 +22,31 @@ Tensors can also be used on a GPU to accelerate computing.
 
 from __future__ import print_function
 import torch
-
+#%%
 ###############################################################
 # Construct a 5x3 matrix, uninitialized:
 
 x = torch.empty(5, 3)
 print(x)
-
+#%%
 ###############################################################
 # Construct a randomly initialized matrix:
-
+#%%
 x = torch.rand(5, 3)
 print(x)
-
+#%%
 ###############################################################
 # Construct a matrix filled zeros and of dtype long:
 
 x = torch.zeros(5, 3, dtype=torch.long)
 print(x)
-
+#%%
 ###############################################################
 # Construct a tensor directly from data:
 
 x = torch.tensor([5.5, 3])
 print(x)
-
+#%%
 ###############################################################
 # or create a tensor based on an existing tensor. These methods
 # will reuse properties of the input tensor, e.g. dtype, unless
@@ -54,15 +54,15 @@ print(x)
 
 x = x.new_ones(5, 3, dtype=torch.double)      # new_* methods take in sizes
 print(x)
-
+#%%
 x = torch.randn_like(x, dtype=torch.float)    # override dtype!
 print(x)                                      # result has the same size
-
+#%%
 ###############################################################
 # Get its size:
 
 print(x.size())
-
+#%%
 ###############################################################
 # .. note::
 #     ``torch.Size`` is in fact a tuple, so it supports all tuple operations.
@@ -75,25 +75,25 @@ print(x.size())
 # Addition: syntax 1
 y = torch.rand(5, 3)
 print(x + y)
-
+#%%
 ###############################################################
 # Addition: syntax 2
 
 print(torch.add(x, y))
-
+#%%
 ###############################################################
 # Addition: providing an output tensor as argument
 result = torch.empty(5, 3)
 torch.add(x, y, out=result)
 print(result)
-
+#%%
 ###############################################################
 # Addition: in-place
 
 # adds x to y
 y.add_(x)
 print(y)
-
+#%%
 ###############################################################
 # .. note::
 #     Any operation that mutates a tensor in-place is post-fixed with an ``_``.
@@ -102,21 +102,21 @@ print(y)
 # You can use standard NumPy-like indexing with all bells and whistles!
 
 print(x[:, 1])
-
+#%%
 ###############################################################
 # Resizing: If you want to resize/reshape tensor, you can use ``torch.view``:
 x = torch.randn(4, 4)
 y = x.view(16)
 z = x.view(-1, 8)  # the size -1 is inferred from other dimensions
 print(x.size(), y.size(), z.size())
-
+#%%
 ###############################################################
 # If you have a one element tensor, use ``.item()`` to get the value as a
 # Python number
 x = torch.randn(1)
 print(x)
 print(x.item())
-
+#%%
 ###############################################################
 # **Read later:**
 #
@@ -136,23 +136,23 @@ print(x.item())
 #
 # Converting a Torch Tensor to a NumPy Array
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
+#%%
 a = torch.ones(5)
 print(a)
-
+#%%
 ###############################################################
 #
 
 b = a.numpy()
 print(b)
-
+#%%
 ###############################################################
 # See how the numpy array changed in value.
 
 a.add_(1)
 print(a)
 print(b)
-
+#%%
 ###############################################################
 # Converting NumPy Array to Torch Tensor
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -164,7 +164,7 @@ b = torch.from_numpy(a)
 np.add(a, 1, out=a)
 print(a)
 print(b)
-
+#%%
 ###############################################################
 # All the Tensors on the CPU except a CharTensor support converting to
 # NumPy and back.
